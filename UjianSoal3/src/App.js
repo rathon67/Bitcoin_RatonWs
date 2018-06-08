e6ff7dad97a2aca8cdee1d01def597c327b12e99
rathon <rathonvespa@gmail.com>
2018-06-08 14:47:11 +0700

    update
    soal3

--- UjianSoal3/src/App.js
+++ UjianSoal3/src/App.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
-import Header from './component/Header';
-import Footer1 from './component/Footer1';
-import Konten from './component/Konten';
-import CetailProduct from './component/Cetailproduct';
-import Cart from './component/Cart';
-import Editprofile from './component/Editprofile';
-import Invoice from './component/Invoice';
-import Invoicehistory from './component/Invoicehistory';
-import Listproduct from './component/Listproduct';
-import Login from './component/Login';
-import Register from './component/Register';
-import Profile from './component/Profile';
-import Checkout from './component/Checkout';
-
+import Output from './komponen/output';
+import Today from './komponen/today';
+import Bitkerup from './komponen/bitkerup';
 
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Route exact path="/index" component={Konten}/>
-        <Route path="/cetail" component={CetailProduct}/>
-        <Route path ="/cart" component={Cart}/>
-        <Route path ="/editprofile" component={Editprofile}/> 
-        <Route path ="/profile" component={Profile}/> 
-        <Route path ="/invoice" component={Invoice}/> 
-        <Route path ="/invoicehistory" component={Invoicehistory}/> 
-        <Route path ="/listproduct" component={Listproduct}/> 
-        <Route path ="/login" component={Login}/> 
-        <Route path ="/register" component={Register}/> 
-        <Route path ="/checkout" component={Checkout}/> 
-     
+  
 
-        <Footer1/>
-      </div>
-    );
-  }
+    render() {
+        return(
+            <div className="App" style={{backgroundColor: 'Green'}}>
+          
+           
+            <center>
+              
+              <div className="tabmenu" style={{width:"700px",margin:"200px",background:"white"}}>
+
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                  <li className="nav-item">
+                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true" style={{fontFamily:'fantasy',color:'green'}}>Bitcoin hari ini</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile2" role="tab" aria-controls="profile" aria-selected="false" style={{fontFamily:'fantasy',color:'green'}}>Rupiah ke bitcoin</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile1" role="tab" aria-controls="profile" aria-selected="false" style={{fontFamily:'fantasy',color:'green'}}>Bitcoin ke rupiah</a>
+                  </li>
+                </ul>
+
+                <div className="tab-content" id="myTabContent">
+                  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div className="login">
+                    <Output />
+                     
+                    </div>
+                  </div>
+                  <div className="tab-pane fade" id="profile2" role="tabpanel" aria-labelledby="profile-tab">
+                  <Today />
+                  </div>
+
+                  <div className="tab-pane fade" id="profile1" role="tabpanel" aria-labelledby="profile-tab">
+                    <Bitkerup/>
+                  </div>
+                </div>
+              
+              </div>
+            </center>
+            </div>
+        )
+    }
 }
 
 export default App;
